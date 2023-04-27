c346ef48595f3abf9aaf323ffb79c2500383f7a2
Monica Liu <liuqianmonica@gmail.com>
2023-04-26 17:48:41 -0700

    resolve conflict

--- src/test/java/com/example/demo/FetchApi.java
+++ src/test/java/com/example/demo/FetchApi.java
@@ -1,13 +1,8 @@
 package com.example.demo;
-
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -18,31 +13,26 @@ import org.springframework.http.HttpHeaders;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
-//import java.net.http.HttpHeaders;
-
-
-
-//@ComponentScan(basePackageClasses = AppConfig.class)
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class FetchApi {
+public class FetchApi {
 
-	@Value("${my.api.token}")
-	private String myApiToken;
+    @Value("${my.api.token}")
+    private String myApiToken;
 
-	@Test
-	void getRequest() throws UnsupportedEncodingException {
-		String location = "New York City";
-		String url = "https://api.yelp.com/v3/businesses/search?location=" + URLEncoder.encode(location, "UTF-8");
+    @Test
+    public void getRequest() throws UnsupportedEncodingException {
+        String location = "New York City";
+        String url = "https://api.yelp.com/v3/businesses/search?location=" + URLEncoder.encode(location, "UTF-8");
 
-		RestTemplate restTemplate = new RestTemplate();
-		HttpHeaders headers = new HttpHeaders();
-		headers.setBearerAuth(myApiToken);
-		HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
-		ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-		String responseBody = response.getBody();
-		System.out.println(responseBody);
-	}
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(myApiToken);
+        HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+        String responseBody = response.getBody();
+        System.out.println(responseBody);
+    }
 }
